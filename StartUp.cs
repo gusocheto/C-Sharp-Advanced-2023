1df7fdabeabd1c161529e91d3847e0c3b19545a2
Hristo Angelov <114013870+gusocheto@users.noreply.github.com>
2023-10-04 21:56:32 +0300

    First steps
    test

--- StartUp.cs
+++ StartUp.cs
@@ -1,24 +1,15 @@
-﻿using RawData;
+﻿namespace CarManufacturer
 
-List<Car> cars = new();
-int count = int.Parse(Console.ReadLine());
-for (int i = 0; i < count; i++)
 {
-    string[] properties = Console.ReadLine().Split(" ").ToArray();
-    Car car = new(properties[0], int.Parse(properties[1]), int.Parse(properties[2]), int.Parse(properties[3]), properties[4], double.Parse(properties[5]), int.Parse(properties[6]), double.Parse(properties[7]), int.Parse(properties[8]), double.Parse(properties[9]), int.Parse(properties[10]), double.Parse(properties[11]), int.Parse(properties[12]));
-    cars.Add(car);
-}
-string command = Console.ReadLine();
+    public class StartUp
+    {
+        static void Main()
+        {
+            Car car = new Car("BMW", "X3", 2006, 500, 300);
+            
+            car.Drive(5);
+            Console.WriteLine(car.WhoAmI());
 
-string[] filterCarModels;
-
-if (command == "fragile")
-{
-    filterCarModels = cars.Where(c => c.Cargo.Type == "fragile" && c.Tyres.Any(t => t.Preasure < 1)).Select(c => c.Model).ToArray();
+        }
+    }
 }
-else
-{
-    filterCarModels = cars.Where(c => c.Cargo.Type == "flammable" && c.Engine.Power > 250).Select(c => c.Model).ToArray();
-}
-
-Console.WriteLine($"{string.Join(Environment.NewLine, filterCarModels)}");
