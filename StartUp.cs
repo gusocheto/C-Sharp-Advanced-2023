edd068e6d5565df6d41e97d10c77d4261be8d2ae
Hristo Angelov <114013870+gusocheto@users.noreply.github.com>
2023-10-04 22:01:28 +0300

    other

--- StartUp.cs
+++ StartUp.cs
@@ -1,15 +1,24 @@
-﻿namespace CarManufacturer
+﻿using DefiningClasses;
+using StartUp;
 
+Family family = new Family();
+int n = int.Parse(Console.ReadLine());
+for (int i = 0; i < n; i++)
 {
-    public class StartUp
-    {
-        static void Main()
-        {
-            Car car = new Car("BMW", "X3", 2006, 500, 300);
-            
-            car.Drive(5);
-            Console.WriteLine(car.WhoAmI());
-
-        }
-    }
+    string[] nameAndAge = Console.ReadLine().Split(" ");
+    string name = nameAndAge[0];
+    int age = int.Parse(nameAndAge[1]);
+    Person person = new Person(name, age);
+    family.AddMember(person);
 }
+Person[] filteredPeople = family.People
+                .FindAll(p => p.Age > 30)
+                .OrderBy(p => p.Name)
+                .ToArray();
+
+foreach (Person person in filteredPeople)
+{
+    Console.WriteLine(person);
+}
+    
+
