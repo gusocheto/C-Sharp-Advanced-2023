e82035f3b9e61a8d80d4247cf0a244bd425c89ea
Hristo Angelov <114013870+gusocheto@users.noreply.github.com>
2023-10-04 22:02:43 +0300

    other (#2)

---
+++ Family.cs
@@ -0,0 +1,35 @@
+﻿using StartUp;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DefiningClasses
+{
+    internal class Family
+    {
+
+        private List<Person> people;
+        public List<Person> People
+        {
+            get { return people; }
+            set { people = value; }
+        }
+
+        public Family()
+        {
+            People = new List<Person>();
+        }
+
+        public void AddMember(Person person)
+        {
+            People.Add(person);
+        }
+
+        public Person GetOldestMember()
+         => People.OrderByDescending(x => x.Age).First();
+            
+        
+    }
+}
